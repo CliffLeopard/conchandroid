bc4be1d4ec7674318eb04de2d12072e427b691a4
CliffLeopard <cliffleopard@yahoo.com>
2023-12-21 10:50:12 +0800

    update

--- app/src/main/java/com/cliff/conch/ConchApplication.kt
+++ app/src/main/java/com/cliff/conch/ConchApplication.kt
@@ -18,23 +18,23 @@ class ConchApplication : MultiDexApplication() {
         Logger.d("ConchApplication OnCreate")
         initEventBus()
     }
+}
 
-    @Suppress("KotlinConstantConditions")
-    private fun under28Init() {
-        if (BuildConfig.FLAVOR == "minSdk28") return
-        Logger.addLogAdapter(object : AndroidLogAdapter() {
-            override fun isLoggable(priority: Int, tag: String?): Boolean {
+@Suppress("KotlinConstantConditions")
+private fun under28Init() {
+    if (BuildConfig.FLAVOR == "minSdk28") return
+    Logger.addLogAdapter(object : AndroidLogAdapter() {
+        override fun isLoggable(priority: Int, tag: String?): Boolean {
 //                return BuildConfig.DEBUG
-                return true
-            }
-        })
-    }
+            return true
+        }
+    })
+}
 
-    // 这里是EventBus3.0改版之后必须要设置的步骤，只有这样EventBus注解生成的SubscriberInfoIndex才能起作用
-    // 一般在Application种初始化的时候调用
-    private fun initEventBus() {
-        EventBus.builder()
-            .addIndex(MyEventBusIndex())
-            .installDefaultEventBus()
-    }
+// 这里是EventBus3.0改版之后必须要设置的步骤，只有这样EventBus注解生成的SubscriberInfoIndex才能起作用
+// 一般在Application种初始化的时候调用
+private fun initEventBus() {
+    EventBus.builder()
+        .addIndex(MyEventBusIndex())
+        .installDefaultEventBus()
 }
